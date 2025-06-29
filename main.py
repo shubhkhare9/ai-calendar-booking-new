bd0f4dd355601a86e4b49db7a4f78e69e08c84ea
shubhkhare9 <shubhkhare.sk11@gmail.com>
2025-06-29 23:36:46 +0530

    🐛 Fixed Request() scope error during token refresh

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from google.auth.transport.requests import Request as GoogleRequest
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
 from pydantic import BaseModel
